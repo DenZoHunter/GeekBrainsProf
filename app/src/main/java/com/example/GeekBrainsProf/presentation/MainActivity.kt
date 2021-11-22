23a714b0d5ddc3e6b689cfd26c9992df50a9b0ac
Denis <denzohunter@bk.ru>
2021-11-22 18:49:37 +0300

    hw7

---
+++ app/src/main/java/com/example/GeekBrainsProf/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.GeekBrainsProf.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import com.example.GeekBrainsProf.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private var navController: NavController? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//
+//        navController = findNavController(this, R.id.main_nav)
+//
+//        setupActionBarWithNavController(this, navController!!)
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController?.navigateUp() == true || super.onSupportNavigateUp()
+//    }
+}
